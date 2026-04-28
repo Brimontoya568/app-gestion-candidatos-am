bbe212557b225c5591d0939974fd73b64ef98313
jaimezzapata <zapataval2304@gmail.com>
2026-04-28 09:42:32 -0500

    Petición de tipo GET en Offers y Candidates

--- src/pages/Candidates.jsx
+++ src/pages/Candidates.jsx
@@ -1,9 +1,26 @@
-function Candidates(){
-    return(
-        <div>
-            <h2>Candidtaos</h2>
-        </div>
-    )
+import { useState, useEffect } from "react";
+import { end_points } from "../services/api";
+
+function Candidates() {
+  const [candidates, setCandidates] = useState([]);
+
+  function getCandidates() {
+    fetch(end_points.candidates)
+      .then((response) => response.json())
+      .then((data) => setCandidates(data))
+      .catch((error) => console.log(error));
+  }
+
+  useEffect(() => {
+    getCandidates();
+  }, []);
+
+  console.log(candidates);
+  return (
+    <div>
+      <h2>Candidtaos</h2>
+    </div>
+  );
 }
 
-export default Candidates
+export default Candidates;
